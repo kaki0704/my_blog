c323eb852abb2baadd9d93ca8dc5c604be779664
yamady0704 <fleis_preis@yahoo.co.jp>
2020-02-03 07:22:15 +0900

    fix

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -2,15 +2,30 @@
   <Layout>
     <h1>#{{$page.tag.title}}</h1>
     <article v-for="edge in $page.tag.belongsTo.edges" :key="edge.node.id" style="margin-bottom: 2em;">
-      <!-- <div v-html="edge.node.content"/> -->
-      <g-image :src="edge.node.cover_image" style="width: 100%"/>
-      <h2>{{edge.node.title}}</h2>
-      <p>{{edge.node.except}}</p>
-      <p>Posted {{edge.node.date}} ・ {{edge.node.timeToRead}} min read</p>
-      <div>
-        <g-link style="padding-right: .25em" v-for="tag in edge.node.tags" :to="tag.path" :key="tag.id"><md-button class="md-raised">{{tag.id}}</md-button></g-link>
-      </div>
-      <g-link :to="edge.node.path">Read Post</g-link>
+      <md-card>
+        <md-card-media>
+          <md-ripple>
+            <g-image :src="edge.node.cover_image" style="width: 100%"/>
+          </md-ripple>
+        </md-card-media>
+        <md-card-header>
+          <div class="md-title">
+            {{edge.node.title}}
+          </div>
+          <div class="md-subhead">
+            <p>Posted {{edge.node.date}} ・ {{edge.node.timeToRead}} min read</p>
+          </div>
+        </md-card-header>
+        <md-card-content>
+          {{edge.node.except}}
+        </md-card-content>
+        <md-card-actions>
+          <g-link :to="edge.node.path"><md-button>Read Post >></md-button></g-link>
+        </md-card-actions>
+        <md-card-actions>
+          <g-link style="padding-right: .25em" v-for="tag in edge.node.tags" :to="tag.path" :key="tag.id"><md-button class="md-raised" :md-ripple="false">{{tag.id}}</md-button></g-link>
+        </md-card-actions>
+      </md-card>
     </article>
   </Layout>
 </template>
